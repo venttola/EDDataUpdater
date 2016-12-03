78b49b678f6658662ef102a5fcb614112f592235
Jukka-Pekka Venttola <jukka-pekka.venttola@student.tut.fi>
2016-12-03 20:39:43 +0200

    Got the async function to work. Added basis for neighbour calculation

--- app/src/routes/system.ts
+++ app/src/routes/system.ts
@@ -6,6 +6,7 @@ import * as express from "express";
 import * as http from "http";
 import * as https from "https";
 import * as WebRequest from "web-request";
+//import Promise from "ts-promise";
 
 
 const EDSM_URL_BASE_OLD : string = "https://www.edsm.net/api-v1/";
@@ -13,47 +14,41 @@ const EDSM_URL_BASE_NEW : string = "";
 
 
 module Routes {
+
+  interface Coordinate {
+      x: "00.000";
+      y: "00.000";
+      z: "00.000";
+  }
   export class SystemRoute {
 
     public async checkSystemValidity(req: express.Request, res: express.Response, next: express.NextFunction) {
-    	/*
-		console.log("Checking is system " + req.params.name + " is valid.");
-    	var parameter : string = "systems?systemName=" + req.params.name;
-     	var result = await WebRequest.get(EDSM_URL_BASE_OLD + parameter);
-
-     	console.log(result.content);
-  	 	res.send(result.content);
-		*/
-	//	console.log("Return dummy data");
-	//	let nearestSystems = [{"name": "System A"}, {"name": "System B"}];
-	//	res.header("Content-Type", "application/json");
-	//	res.send(JSON.stringify(nearestSystems));
 
       console.log("Checking is system " + req.params.name + " is valid.");
       var parameter : string = "system?systemName=" + req.params.name + "\n";
-       var result = await WebRequest.get(EDSM_URL_BASE_OLD + parameter);
+      var result = await WebRequest.get(EDSM_URL_BASE_OLD + parameter);
       if (result.content === "[]") {
         console.log("No system found!");
         res.status(404).send({error: "System not found!"});
       } else {
         console.log("Return dummy data");
-        let nearestSystems = [{"name": "System A"}, {"name": "System B"}];
+        console.log("Getting nearest neighbours for " + req.params.name );
+        //Needs some system here to actually calculate the nearest ones 
+        let nearestSystems = await SystemRoute.prototype.getNearest(req.params.name);
         res.header("Content-Type", "application/json");
-        console.log(result.content);
-        res.send(JSON.stringify(nearestSystems));
+        res.send(nearestSystems);
      }
-
     }
-
-    private async getNearestNeighbours(req: express.Request, res: express.Response, next: express.NextFunction) {
-    	console.log("Getting " + req.params.amount + "nearest neighbours for" + req.params.name);
-    	var parameter : string = "sphere-systems?systemName" + req.params.name + "&radius=20" + "&coods=1";
-    	var result = await WebRequest.get(EDSM_URL_BASE_OLD + parameter);
-    	console.log(result.content);
-    	//Add here a parsing function to figure out n nearest neighbours.
-    	res.send(result.content);
-
-    }
+     private async getNearest(system: string): Promise<string> {
+          //DoStull
+        var parameter : string = "sphere-systems?systemName" + system + "&radius=10" + "&coords=1";
+        let result = await WebRequest.json<any>(EDSM_URL_BASE_OLD + parameter);
+        console.log(result);
+       for (var quote of result){
+          console.log(quote.name);
+       }
+        return JSON.stringify([{"system1": "System1", "system2": "System2"}]);
+      }
   }
 
 }
