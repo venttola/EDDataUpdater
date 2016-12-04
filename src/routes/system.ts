5a2a03340e7fe328ed2898f0cf22c30d53e1ab25
Jukka-Pekka Venttola <jukka-pekka.venttola@student.tut.fi>
2016-12-04 20:47:59 +0200

    api/systems/:name now returns 3 nearest systems and their distances from the queried system

--- src/routes/system.ts
+++ src/routes/system.ts
@@ -11,13 +11,20 @@ import * as WebRequest from "web-request";
 
 const EDSM_URL_BASE_OLD : string = "https://www.edsm.net/api-v1/";
 const EDSM_URL_BASE_NEW : string = "";
-
+const AMOUNT_OF_NEAREST : number = 3;
 module Routes {
 
-  interface Coordinate {
-      x: "00.000";
-      y: "00.000";
-      z: "00.000";
+ interface SystemWithDistance {
+      name: string;
+      distance: number;
+  }
+  interface SystemWithCoordinates {
+    name: string;
+    coords: {
+      x: number;
+      y: number;
+      z: number;
+    };
   }
   export class SystemRoute {
 
@@ -48,15 +55,68 @@ module Routes {
         res.send(nearestSystems);
      }
     }
-     private async getNearest(system: string): Promise<string> {
+     private async getNearest(systemName: string): Promise<string> {
           //DoStull
-        var parameter : string = "sphere-systems?systemName" + system + "&radius=10" + "&coords=1";
+        var parameter : string = "sphere-systems?systemName" + systemName + "&radius=10" + "&coords=1";
         let result = await WebRequest.json<any>(EDSM_URL_BASE_OLD + parameter);
-        console.log(result);
-       for (var quote of result){
-          console.log(quote.name);
-       }
-        return JSON.stringify([{"system1": "System1", "system2": "System2"}]);
+       // console.log(result);
+       let base: SystemWithCoordinates;
+        for (var quote of result) {
+         // console.log(quote.name);
+          if (quote.name === systemName) {
+              base = quote;
+          }
+        }
+        console.log(base);
+        let systemList: SystemWithDistance[] = [];
+        for (var quote of result) {
+          console.log("Checking system " + quote.name);
+          let distance = await this.calculateDistance(
+            Number(base.coords.x),
+            Number(base.coords.y),
+            Number(base.coords.z),
+            Number(quote.coords.x),
+            Number(quote.coords.y),
+            Number(quote.coords.z));
+          if ((systemList.length === 0 || systemList.length < AMOUNT_OF_NEAREST) && quote.name !== systemName) {
+            let nearSystem: SystemWithDistance = {name: quote.name, distance: distance };
+            systemList.push(nearSystem);
+
+          } else if (quote.name !== systemName) {
+            for (let system of systemList) {
+               if (distance < system.distance && quote.name !== system.name) {
+                 let nearSystem: SystemWithDistance = {name: quote.name, distance: distance };
+                 systemList.push(nearSystem);
+                 systemList = systemList.sort((system1, system2) => {
+                   if (system1.distance > system2.distance) {
+                     return 1;
+                   }
+                   if (system1.distance < system2.distance) {
+                     return -1;
+                   }
+                   return 0;
+                   });
+                 systemList.pop();
+                 break;
+               }
+            }
+          }
+          systemList = systemList.sort((system1, system2) => {
+            if (system1.distance > system2.distance) {
+              return 1;
+            }
+            if (system1.distance < system2.distance) {
+              return -1;
+            }
+            return 0;
+          });
+          console.log(JSON.stringify(systemList));
+          }
+     //  console.log (JSON.stringify(systemList));
+        return JSON.stringify(JSON.stringify(systemList));
+      }
+      private async calculateDistance(x1: number, y1: number, z1: number, x2: number, y2: number, z2: number): Promise<number> {
+        return Number(Math.sqrt( (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2)).toFixed(2));
       }
   }
 
